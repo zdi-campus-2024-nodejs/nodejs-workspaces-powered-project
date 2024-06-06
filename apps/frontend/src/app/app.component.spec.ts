aa6f1e616650fe09a7e42524d49ccd0b36cf75ea
zoltan.szabo <zoltan.szabo.it@gmail.com>
2024-06-06 11:30:54 +0200

    fix tests

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -20,10 +21,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('nodejs-workspaces-powered-project');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nodejs-workspaces-powered-project');
-  });
 });
